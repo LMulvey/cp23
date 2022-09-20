f7b3596930f53ba10004fcecdd8289a421eb2ef4
Lee Mulvey <lmulvey@me.com>
2022-09-19 20:24:18 -0600

    fix: type issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { PageTitle } from '@/components/PageTitle';
 import { styled } from '@/stitches';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Image from 'next/image';
 
 const Container = styled('div', {
