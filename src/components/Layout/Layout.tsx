6ad8f6672913b2464dcb2934ef1fdf87d3d7de33
Lee Mulvey <lmulvey@me.com>
2023-02-11 11:50:38 -0700

    feat: new navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,22 +10,23 @@ import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { PageTitle } from '../PageTitle';
 import { Content } from '../Typography';
+import { BookOpenCheck, Plane, Star } from 'lucide-react';
 
 const Container = styled(motion.div, {
   width: '100vw',
   display: 'flex',
   flexFlow: 'column nowrap',
   alignItems: 'center',
-  padding: '$24 $36',
+  padding: '0 $36',
   gap: '$8',
   '@bp2': {
-    padding: '$48 $124',
+    padding: '0 $124',
   },
 });
 
 const Heading = styled(motion.h1, {
   textShadow: '$blue40',
-  fontSize: '$32',
+  fontSize: '$18',
   fontWeight: '700',
   letterSpacing: '-$1',
   lineHeight: 1,
@@ -43,33 +44,32 @@ const Gradient = styled(motion.span, {
 
 const Links = styled(motion.div, {
   display: 'flex',
-  flexFlow: 'column nowrap',
+  flexFlow: 'row wrap',
   gap: '$16',
-  margin: '$48 0',
   justifyContent: 'center',
   alignItems: 'center',
-  width: '100vw',
-  padding: '$36 0',
-  background: '$pinkGradient2',
-  boxShadow: '$blue40',
   '@bp2': {
-    flexFlow: 'row nowrap',
-    gap: '$36',
-    padding: '$36 0',
+    justifyContent: 'start',
+  },
+  '@bp3': {
+    gap: '$18',
+  },
+  '@bp4': {
+    gap: '$32',
+  },
+  variants: {
+    hasDisabledLinks: {
+      true: {
+        marginBottom: '$8',
+      },
+    },
   },
 });
 
-// const LinkDivider = styled(motion.div, {
-//   width: '4px',
-//   height: '100%',
-//   borderRadius: '6px',
-//   background: '$pinkGradient',
-// });
-
 const Link = styled(motion.a, {
-  color: '$white',
+  color: '#967275',
   textShadow: '$blue40',
-  fontSize: '$24',
+  fontSize: '$18',
   fontWeight: '600',
   textAlign: 'center',
   letterSpacing: '-$1',
@@ -77,37 +77,30 @@ const Link = styled(motion.a, {
   marginTop: '-$12',
   position: 'relative',
   textDecoration: 'none',
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
 
-  '&:after': {
-    content: '',
-    position: 'absolute',
-    bottom: '-$12',
-    left: '50%',
-    height: '$4',
-    transformOrigin: 'center center',
-    background: '$yellow',
-    borderRadius: '$8',
-    boxShadow: '$blue100',
-    transform: 'scaleX(1) translateX(-50%)',
-    transition: 'transform 0.2s ease-out',
-    width: '100%',
+  '& svg': {
+    marginRight: '$8',
   },
+
+  '&[aria-disabled="true"]': {
+    textDecoration: 'line-through',
+    position: 'relative',
+    color: 'rgba(150, 114, 117, 0.3)',
+    pointerEvents: 'none',
+  },
+
   variants: {
     isActive: {
       true: {
-        '&:after': {
-          transform: 'scaleX(1) translateX(-50%)',
-          background: '$white',
-          '@bp2': {
-            transform: 'scaleX(1)',
-            background: '$white',
-          },
-        },
+        color: '#e18a92',
       },
     },
   },
   '@bp2': {
-    fontSize: '$28',
     textAlign: 'left',
     marginTop: '-$4',
     '&:hover': {
@@ -116,11 +109,41 @@ const Link = styled(motion.a, {
       },
     },
     '&:after': {
-      width: 'calc(100% + $24)',
       left: 0,
       transform: 'scaleX(0)',
+      content: '',
+      position: 'absolute',
+      bottom: '-$8',
+      height: '$4',
+      transformOrigin: 'center center',
+      background: '$yellow',
+      boxShadow: '$blue100',
+      transition: 'transform 125ms ease-out',
+      width: '100%',
     },
   },
+});
+
+const LinkDisabled = styled('p', {
+  position: 'absolute',
+  bottom: '-16px',
+  right: 0,
+  background: '$pinkGradient2',
+  boxShadow: '$blue20',
+  color: '$white',
+  opacity: 0.8,
+  fontSize: '$8',
+  textTransform: 'uppercase',
+  fontWeight: 700,
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '$8',
+  padding: '$4 $8',
+  whiteSpace: 'nowrap',
+  '-webkit-backdrop-filter': 'blur(8px)',
+  backDropFilter: 'blur(8px)',
 });
 
 const ImageContainer = styled(motion.div, {
@@ -129,18 +152,46 @@ const ImageContainer = styled(motion.div, {
   },
 });
 
-const Navbar = styled('div', {
+const Navbar = styled(motion.div, {
   display: 'flex',
   flexFlow: 'column nowrap',
   justifyContent: 'center',
   alignItems: 'center',
   width: '100vw',
-  padding: '0 $8',
-  gap: '$32',
-  '@bp2': {
+  padding: '$24 $8',
+  height: '100%',
+  gap: '$16',
+  boxShadow: '$blue40',
+  background: '#fdf9df',
+  marginBottom: '$32',
+  position: 'relative',
+  '@bp3': {
     gap: '$8',
+    height: '$124',
     padding: '0 $32',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    flexFlow: 'row nowrap',
+  },
+  '&::before': {
+    content: '',
+    width: '100vw',
+    height: '$8',
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    background: '$pinkGradient2',
+  },
+});
+
+const LogoAndLinks = styled('div', {
+  display: 'flex',
+  flexFlow: 'column nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '$24',
+  '@bp3': {
+    gap: '$72',
     justifyContent: 'space-between',
     flexFlow: 'row nowrap',
   },
@@ -172,51 +223,99 @@ export const Layout = ({
     <Container {...props}>
       <PageTitle title={title} />
 
-      <Navbar>
-        <NextLink href="/">
-          <ImageContainer variants={commonAnimationVariants}>
-            <Image
-              alt="Console-ing Passions logo"
-              src="/images/cp-logo.png"
-              width="427px"
-              height="115px"
-              layout="intrinsic"
-              priority
-            />
-          </ImageContainer>
-        </NextLink>
+      <Navbar variants={commonAnimationVariants}>
+        <LogoAndLinks>
+          <NextLink href="/">
+            <ImageContainer>
+              <Image
+                alt="Console-ing Passions logo"
+                src="/images/cp-logo.png"
+                width="213px"
+                height="57px"
+                layout="intrinsic"
+                priority
+              />
+            </ImageContainer>
+          </NextLink>
+
+          <Links hasDisabledLinks>
+            <NextLink href="/" passHref>
+              <Link
+                aria-label="Console-ing Passions 2023 Homepage"
+                isActive={isActive('/')}
+                href="/"
+              >
+                Home
+              </Link>
+            </NextLink>
+            <NextLink href="/plan" passHref>
+              <Link
+                aria-disabled="true"
+                aria-label="Plan Your Trip to Console-ing Passions 2023"
+                isActive={isActive('/call')}
+                href="/plan"
+              >
+                <LinkDisabled>Coming Soon</LinkDisabled>
+                <Plane />
+                Plan Your Trip
+              </Link>
+            </NextLink>
+            <NextLink href="/keynotes" passHref>
+              <Link
+                aria-disabled="true"
+                aria-label="Keynotes attending Console-ing Passions 2023"
+                isActive={isActive('/keynotes')}
+                href="/keynotes"
+              >
+                <LinkDisabled>Coming Soon</LinkDisabled>
+                <Star />
+                Keynotes
+              </Link>
+            </NextLink>
+            <NextLink href="/call-for-papers" passHref>
+              <Link
+                aria-disabled="true"
+                aria-label="Console-ing Passions 2023 Call for Papers Submission"
+                isActive={isActive('/call-for-papers')}
+                href="/call-for-papers"
+              >
+                <LinkDisabled>Now Closed</LinkDisabled>
+                <BookOpenCheck />
+                Call for Papers
+              </Link>
+            </NextLink>
+          </Links>
+        </LogoAndLinks>
         <NavbarInfo>
-          <Heading variants={commonAnimationVariants}>
-            June 22 - 24, 2023<Gradient>.</Gradient>
-          </Heading>
-          <Heading variants={commonAnimationVariants}>
-            Univers<Gradient>ity of Calgary</Gradient>.
+          <Heading css={{ fontSize: '$24' }}>June 22 - 24, 2023</Heading>
+          <Heading
+            as={motion.a}
+            css={{
+              textDecoration: 'none',
+            }}
+            href="https://ucalgary.ca"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Gradient>@ University of Calgary</Gradient>
           </Heading>
         </NavbarInfo>
       </Navbar>
-      <Links variants={commonAnimationVariants}>
-        <NextLink href="/call-for-papers" passHref>
-          <Link
-            aria-label="Console-ing Passions 2023 Call for Papers Submission"
-            isActive={isActive('/call-for-papers')}
-            href="/call-for-papers"
-          >
-            🔖 Call for Papers
-          </Link>
-        </NextLink>
-        {/* <LinkDivider />
-        <Link>🤔 FAQ</Link> */}
-      </Links>
+
       {children}
+
       <Footer variants={commonAnimationVariants}>
+        {/* cSpell:disable */}
         <Content footer variants={contentVariants}>
           Calgary is located in the traditional territories of the people of the
           Treaty 7 region in Southern Alberta, which includes the Blackfoot
           Confederacy (comprising the Siksika, Piikani, and Kainai First
-          Nations), as well as the Tsuut’ina First Nation, and the Stoney Nakoda
-          (including the Chiniki, Bearspaw, and Wesley First Nations). The city
-          of Calgary is also home to the Métis Nation of Alberta, Region 3.
+          Nations), as well as the Tsuut&apos;ina First Nation, and the Stoney
+          Nakoda (including the Chiniki, Bearspaw, and Wesley First Nations).
+          The city of Calgary is also home to the Métis Nation of Alberta,
+          Region 3.
         </Content>
+        {/* cSpell:enable */}
       </Footer>
     </Container>
   );
--- src/utilities/isCFPOpen.ts
+++
@@ -1,5 +0,0 @@
-import dayjs from './dayjs';
-
-export const isCFPOpen = () => {
-  return dayjs().isBefore('2023-01-16 23:59:59');
-};
